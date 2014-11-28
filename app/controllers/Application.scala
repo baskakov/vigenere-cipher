5f19d29882901d2fd321fd307eb287a2f4a8ac6a
Dmitry Baskakov <dmitry.baskakov.github@gmail.com>
2014-11-28 18:36:15 +0300

    Encoder

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,20 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { request =>
+    val query = request.body.asFormUrlEncoded.getOrElse(Map.empty)
+    val key = query.get("key").flatMap(_.headOption)
+    val message = query.get("message").flatMap(_.headOption)
+    val cipher = key.flatMap(k => message.map(m => {
+        m.foldLeft(("", 0))({
+          case ((xs, i),b) =>
+            val msgChar = Integer.parseInt(b.toString, 16)
+            val keyChar = Integer.parseInt(k.charAt(i%k.size).toString, 16)
+            val xor = Integer.toHexString(msgChar ^ keyChar)
+            (xs ++ xor, i+1)
+        })
+    })).map(_._1.toUpperCase)
+    Ok(views.html.index(key, message, cipher))
   }
 
 }
